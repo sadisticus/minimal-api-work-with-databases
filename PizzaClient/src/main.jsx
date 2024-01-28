7d6acc2bdec35d549c72b9ee914bf8215f13bc9f
sadisticus <100002791+sadisticus@users.noreply.github.com>
2024-01-28 02:58:08 +0000

    minor edit

--- PizzaClient/src/main.jsx
+++ PizzaClient/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Pizza from './Pizza.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <Pizza />
-  </React.StrictMode>,
+  
 )
